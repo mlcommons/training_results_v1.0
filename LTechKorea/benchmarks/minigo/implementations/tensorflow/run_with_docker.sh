239bead0929037331de360fe437883c148a00fae
Jaeyeong Huh <jay.jyhuh@gmail.com>
2021-09-06 15:32:55 +0900

    feat(minigo): #8, #18 add `config_LTKV100S.sh` and use `docker run --gpus=all`
    Signed-off-by: Jaeyeong Huh <jay.jyhuh@gmail.com>

--- LTechKorea/benchmarks/minigo/implementations/tensorflow/run_with_docker.sh
+++ LTechKorea/benchmarks/minigo/implementations/tensorflow/run_with_docker.sh
@@ -55,7 +55,9 @@ cleanup_docker
 trap 'set -eux; cleanup_docker' EXIT
 
 # Setup container
-nvidia-docker run --rm --init --detach \
+#nvidia-docker run --rm --init --detach \
+
+docker run --rm --init --gpus=all --detach \
     --net=host --uts=host --ipc=host --security-opt=seccomp=unconfined \
     --ulimit=stack=67108864 --ulimit=memlock=-1 \
     --name="${_cont_name}" "${_cont_mounts[@]}" \
